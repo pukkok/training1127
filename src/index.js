98a4445cffab6fb7cabb31864b8dcf1d9d65bf83
Pukkok <krystalgggg@gmail.com>
2024-11-29 01:15:54 +0900

    append를 이용한 csr방식 테스트

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './style.css'
 
 console.log('작동 확인')
+const root = document.getElementById('root')
+
+const div = document.createElement('div')
+const h1 = document.createElement('h1')
+h1.innerText = 'hello CSR!'
+div.append(h1)
+root.append(div)
