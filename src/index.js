036e50c4b9ba645459a72a9ab04fedd3d5917d1e
pukkok <krystalgggg@gmail.com>
2024-11-29 09:24:43 +0900

    불러오기 작업 테스트

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css'
 
+import $nt from './component/test1'
+
 console.log('작동 확인')
 const root = document.getElementById('root')
 
-const div = document.createElement('div')
-const h1 = document.createElement('h1')
+const div = $nt('div')
+const h1 = $nt('h1')
 h1.innerText = 'hello CSR!'
 div.append(h1)
 root.append(div)
